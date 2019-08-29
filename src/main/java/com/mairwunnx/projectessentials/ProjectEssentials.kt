b2fb97c2bf9f13986cdec15e8c2f1935a30838ec
Pavel Erokhin <MairwunNx@gmail.com>
2019-08-29 19:43:10 +0300

    Removed Java files.

--- src/main/java/com/mairwunnx/projectessentials/ProjectEssentials.kt
+++ src/main/java/com/mairwunnx/projectessentials/ProjectEssentials.kt
@@ -6,6 +6,7 @@ import com.mairwunnx.projectessentials.proxy.ServerProxy
 import net.alexwells.kottle.FMLKotlinModLoadingContext
 import net.minecraftforge.fml.DistExecutor
 import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 import java.util.function.Supplier
@@ -13,7 +14,7 @@ import java.util.function.Supplier
 const val MOD_ID = "projectessentials"
 
 @Mod(MOD_ID)
-class ProjectEssentialsTemp {
+class ProjectEssentials {
     private val logger: Logger = LogManager.getLogger()
     private val proxy: CommonProxy =
         DistExecutor.runForDist<CommonProxy>(
@@ -22,6 +23,14 @@ class ProjectEssentialsTemp {
         )
 
     init {
+        logger.info("Project Essentials starting initializing ...")
         FMLKotlinModLoadingContext.get().modEventBus.register(proxy)
+        FMLKotlinModLoadingContext.get().modEventBus.addListener<FMLCommonSetupEvent> {
+            this.setup()
+        }
+    }
+
+    private fun setup() {
+        logger.info("Project Essentials starting setup mod ...")
     }
 }
