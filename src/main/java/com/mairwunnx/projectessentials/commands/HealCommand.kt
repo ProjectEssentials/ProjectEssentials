3362515c3ce78bda9dd645222e5b24622f7b471d
Pavel Erokhin <MairwunNx@gmail.com>
2019-08-30 15:21:23 +0300

    Added ability to executing aliases for commands (eheal).
    Added checking on max health for player.
    Added notifty in log if player failed to use heal \ eheal command.

--- src/main/java/com/mairwunnx/projectessentials/commands/HealCommand.kt
+++ src/main/java/com/mairwunnx/projectessentials/commands/HealCommand.kt
@@ -15,50 +15,59 @@ class HealCommand {
         private val logger: Logger = LogManager.getLogger()
         private const val HEAL_COMMAND: String = "heal"
         private const val HEAL_ARG_NAME_COMMAND: String = "player"
+        private val HealCommandAliases: Array<String> = arrayOf(HEAL_COMMAND, "eheal")
 
         fun register(
             dispatcher: CommandDispatcher<CommandSource>
         ) {
             logger.info("Starting register \"/$HEAL_COMMAND\" command ...")
 
-            dispatcher.register(
-                literal<CommandSource>(HEAL_COMMAND)
-                    .then(
-                        argument<CommandSource, String>(HEAL_ARG_NAME_COMMAND, string())
-                            .executes {
-                                execute(it, true)
-                                return@executes 1
-                            }
-                    )
-                    .executes {
-                        execute(it, false)
-                        return@executes 1
-                    }
-            )
+            HealCommandAliases.forEach { command ->
+                dispatcher.register(
+                    literal<CommandSource>(command)
+                        .then(
+                            argument<CommandSource, String>(HEAL_ARG_NAME_COMMAND, string())
+                                .executes {
+                                    execute(it, true)
+                                    return@executes 1
+                                }
+                        )
+                        .executes {
+                            execute(it, false)
+                            return@executes 1
+                        }
+                )
+            }
         }
 
         private fun execute(c: CommandContext<CommandSource>, hasTarget: Boolean) {
             val sender: String = c.source.asPlayer().name.string
 
-            if (!c.source.asPlayer().hasPermissionLevel(2)) return
+            if (!c.source.asPlayer().hasPermissionLevel(2)) {
+                logger.info("Player ($sender) failed to executing \"/$HEAL_COMMAND\" command")
+                // todo: add notification message for player about it.
+                return
+            }
             logger.info("Executed command \"/$HEAL_COMMAND\" from $sender")
 
             if (hasTarget) {
                 val player: String = getString(c, HEAL_ARG_NAME_COMMAND)
                 c.source.world.players.forEach {
                     if (it.name.string != player || it.hasDisconnected()) return
+                    if (it.health == it.maxHealth) return
                     logger.info(
                         "Player ($player) Health changed from ${c.source.asPlayer().health} to ${c.source.asPlayer().maxHealth} by $sender"
                     )
                     it.health = it.maxHealth
+                    // todo: add notification message for player about it.
                 }
             } else {
-                if (c.source.asPlayer().health != c.source.asPlayer().maxHealth) {
-                    logger.info(
-                        "Player ($sender) Health changed from ${c.source.asPlayer().health} to ${c.source.asPlayer().maxHealth}"
-                    )
-                    c.source.asPlayer().health = c.source.asPlayer().maxHealth
-                }
+                if (c.source.asPlayer().health == c.source.asPlayer().maxHealth) return
+                logger.info(
+                    "Player ($sender) Health changed from ${c.source.asPlayer().health} to ${c.source.asPlayer().maxHealth}"
+                )
+                c.source.asPlayer().health = c.source.asPlayer().maxHealth
+                // todo: add notification message for player about it.
             }
         }
     }
