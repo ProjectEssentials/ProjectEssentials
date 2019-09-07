740fb9e805e775e3b65e8589276d19be35eb4635
Pavel Erokhin <MairwunNx@gmail.com>
2019-09-07 13:50:59 +0300

    Added documentation for isPlayerSender extension of CommandContext class.

--- src/main/kotlin/com/mairwunnx/projectessentials/extensions/CommandContextExtensions.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/extensions/CommandContextExtensions.kt
@@ -4,5 +4,8 @@ import com.mojang.brigadier.context.CommandContext
 import net.minecraft.command.CommandSource
 import net.minecraft.entity.player.ServerPlayerEntity
 
+/**
+ * Return true if command sender is player.
+ */
 fun CommandContext<CommandSource>.isPlayerSender(): Boolean =
     this.source.entity is ServerPlayerEntity
