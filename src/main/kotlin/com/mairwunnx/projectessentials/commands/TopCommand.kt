823a7d920cb3c141578fe9de8ee9af050d9e224f
Pavel Erokhin <MairwunNx@gmail.com>
2019-09-14 18:35:48 +0300

    Improved top command code.

--- src/main/kotlin/com/mairwunnx/projectessentials/commands/TopCommand.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/commands/TopCommand.kt
@@ -2,12 +2,13 @@ package com.mairwunnx.projectessentials.commands
 
 import com.mairwunnx.projectessentials.configurations.ModConfiguration
 import com.mairwunnx.projectessentials.extensions.isPlayerSender
+import com.mairwunnx.projectessentials.extensions.sendMsg
+import com.mairwunnx.projectessentials.helpers.ONLY_PLAYER_CAN
 import com.mairwunnx.projectessentials.helpers.PERMISSION_LEVEL
 import com.mojang.brigadier.CommandDispatcher
-import com.mojang.brigadier.builder.LiteralArgumentBuilder
+import com.mojang.brigadier.builder.LiteralArgumentBuilder.literal
 import com.mojang.brigadier.context.CommandContext
 import net.minecraft.command.CommandSource
-import net.minecraft.util.text.TranslationTextComponent
 import net.minecraft.world.gen.Heightmap
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
@@ -18,20 +19,21 @@ import org.apache.logging.log4j.Logger
  * **Usage example:** `/top` and `/etop`.
  *
  * **Available arguments:** none.
+ *
+ * **Can server use it command?:** `false`.
  */
 object TopCommand {
     private val logger: Logger = LogManager.getLogger()
     private const val TOP_COMMAND: String = "top"
-    private val topCommandAliases: MutableList<String> = mutableListOf(TOP_COMMAND)
     private const val topYPosModifier: Double = 1.4
     private const val centerOfBlockPos: Double = 0.5
+    private val topCommandAliases: MutableList<String> = mutableListOf(TOP_COMMAND)
 
     fun register(
         dispatcher: CommandDispatcher<CommandSource>
     ) {
         val modConfig = ModConfiguration.getCommandsConfig()
         logger.info("Starting register \"/$TOP_COMMAND\" command ...")
-        logger.info("Processing commands aliases for \"/$TOP_COMMAND\" command ...")
 
         CommandAliases.aliases[TOP_COMMAND] = modConfig.commands.top.aliases.toMutableList()
         topCommandAliases.addAll(
@@ -39,50 +41,41 @@ object TopCommand {
         )
 
         topCommandAliases.forEach { command ->
-            dispatcher.register(
-                LiteralArgumentBuilder.literal<CommandSource>(command)
-                    .executes {
-                        execute(it)
-                        return@executes 1
-                    }
+            dispatcher.register(literal<CommandSource>(command)
+                .executes {
+                    execute(it)
+                    return@executes 1
+                }
             )
         }
     }
 
     private fun execute(c: CommandContext<CommandSource>) {
         val modConfig = ModConfiguration.getCommandsConfig()
-        if (!c.isPlayerSender()) {
-            logger.warn(
-                "\"/$TOP_COMMAND\" command should only be used by the player!"
-            )
+        val permissionLevel = modConfig.commands.top.permissionLevel
+        val isPlayerSender = c.isPlayerSender()
+
+        if (!isPlayerSender) {
+            logger.warn(ONLY_PLAYER_CAN.replace("%0", TOP_COMMAND))
             return
         }
 
-        val commandSenderNickName: String = c.source.asPlayer().name.string
-        val commandSender: CommandSource = c.source
+        val commandSender = c.source
+        val commandSenderPlayer = commandSender.asPlayer()
+        val commandSenderNickName = commandSenderPlayer.name.string
 
-        if (!commandSender.asPlayer().hasPermissionLevel(
-                modConfig.commands.top.permissionLevel
-            )
-        ) {
+        if (!commandSenderPlayer.hasPermissionLevel(permissionLevel)) {
             logger.warn(
                 PERMISSION_LEVEL
                     .replace("%0", commandSenderNickName)
                     .replace("%1", TOP_COMMAND)
             )
-            commandSender.sendFeedback(
-                TranslationTextComponent(
-                    "project_essentials.top.error"
-                ),
-                true
-            )
+            sendMsg(commandSender, "top.error")
             return
         }
 
-        logger.info("Executed command \"/$TOP_COMMAND\" from $commandSenderNickName")
-
-        val position = commandSender.asPlayer().position
-        val heightTop = commandSender.asPlayer().world
+        val position = commandSenderPlayer.position
+        val heightTop = commandSenderPlayer.world
             .getChunkAt(position)
             .getTopBlockY(
                 Heightmap.Type.MOTION_BLOCKING,
@@ -93,16 +86,12 @@ object TopCommand {
         logger.info(
             "Player ($commandSenderNickName) top pos (y) changed from ${position.y.toDouble()} to $heightTop"
         )
-        commandSender.asPlayer().setPositionAndUpdate(
+        commandSenderPlayer.setPositionAndUpdate(
             position.x + centerOfBlockPos,
             heightTop,
             position.z + centerOfBlockPos
         )
-        commandSender.sendFeedback(
-            TranslationTextComponent(
-                "project_essentials.top.success"
-            ),
-            true
-        )
+        sendMsg(commandSender, "top.success")
+        logger.info("Executed command \"/$TOP_COMMAND\" from $commandSenderNickName")
     }
 }
