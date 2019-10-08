090357925e6b62ea99ff01126b15155370afe615
Pavel Erokhin <MairwunNx@gmail.com>
2019-10-08 18:50:05 +0300

    Removed bypassPermissionLevel property from CooldownsConfig.

--- src/main/kotlin/com/mairwunnx/projectessentials/configurations/CooldownsConfig.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/configurations/CooldownsConfig.kt
@@ -5,8 +5,6 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class CooldownsConfig(
-    @SerialName("BypassPermissionLevel")
-    val bypassPermissionLevel: Int = 2,
     @SerialName("CommandCooldowns")
     val commandCooldowns: MutableList<String> = mutableListOf(),
     @SerialName("IgnoredPlayers")
