670fc586df4004419078de4cd71e2d354c05c3f9
Pavel Erokhin <MairwunNx@gmail.com>
2019-10-09 23:40:04 +0300

    BreakCommand class code refactoring.

--- src/main/kotlin/com/mairwunnx/projectessentials/commands/general/BreakCommand.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/commands/general/BreakCommand.kt
@@ -1,38 +1,41 @@
 package com.mairwunnx.projectessentials.commands.general
 
 import com.mairwunnx.projectessentials.commands.CommandBase
-import com.mairwunnx.projectessentials.configurations.CommandsConfig
 import com.mairwunnx.projectessentials.configurations.ModConfiguration.getCommandsConfig
 import com.mairwunnx.projectessentials.extensions.sendMsg
+import com.mairwunnx.projectessentialscore.helpers.ONLY_PLAYER_CAN
+import com.mairwunnx.projectessentialscore.helpers.PERMISSION_LEVEL
+import com.mairwunnx.projectessentialspermissions.permissions.PermissionsAPI
 import com.mojang.brigadier.CommandDispatcher
 import com.mojang.brigadier.builder.LiteralArgumentBuilder.literal
 import com.mojang.brigadier.context.CommandContext
-import kotlinx.serialization.UnstableDefault
 import net.minecraft.block.material.Material
 import net.minecraft.command.CommandSource
 import net.minecraft.util.math.BlockPos
 import org.apache.logging.log4j.LogManager
 import kotlin.math.roundToInt
 
-@UnstableDefault
-object BreakCommand : CommandBase<CommandsConfig.Commands.Break>(
-    getCommandsConfig().commands.`break`,
-    hasArguments = false
-) {
+object BreakCommand : CommandBase() {
     private val logger = LogManager.getLogger()
+    private var config = getCommandsConfig().commands.`break`
+
+    init {
+        command = "break"
+        aliases = config.aliases.toMutableList()
+    }
 
     override fun reload() {
-        commandInstance = getCommandsConfig().commands.`break`
+        config = getCommandsConfig().commands.`break`
+        aliases = config.aliases.toMutableList()
         super.reload()
     }
 
     override fun register(dispatcher: CommandDispatcher<CommandSource>) {
         super.register(dispatcher)
-        commandAliases.forEach { command ->
+        aliases.forEach { command ->
             dispatcher.register(literal<CommandSource>(command)
                 .executes {
-                    execute(it)
-                    return@executes 0
+                    return@executes execute(it)
                 }
             )
         }
@@ -40,37 +43,48 @@ object BreakCommand : CommandBase<CommandsConfig.Commands.Break>(
 
     override fun execute(
         c: CommandContext<CommandSource>,
-        hasTarget: Boolean
-    ): Boolean {
-        val code = super.execute(c, hasTarget)
-        if (!code) return false
-
-        val maxDistance = 20
-        for (i in 1..maxDistance) {
-            val eyePos = senderPlayer.getEyePosition(1.0f)
-            val lookVec = senderPlayer.getLook(1.0f)
-            val targetObjectPos = eyePos.add(
-                (lookVec.x * i).roundToInt().toDouble(),
-                (lookVec.y * i).roundToInt().toDouble(),
-                (lookVec.z * i).roundToInt().toDouble()
-            )
-            val blockPos = BlockPos(targetObjectPos.x, targetObjectPos.y, targetObjectPos.z)
-            val blockState = senderPlayer.world.getBlockState(blockPos)
-            val blockMaterial = blockState.material
-            if (blockMaterial != Material.AIR) {
-                when {
-                    config.commands.`break`.restrictedBlocks.contains(
-                        blockState.block.registryName.toString()
-                    ) && !senderPlayer.hasPermissionLevel(
-                        config.commands.`break`.restrictedBlockByPassPermLevel
-                    ) -> sendMsg(sender, "break.restricted")
-                    else -> sender.world.destroyBlock(blockPos, false)
+        argument: Any?
+    ): Int {
+        super.execute(c, argument)
+        if (senderIsServer) {
+            logger.warn(ONLY_PLAYER_CAN.replace("%0", command))
+        } else {
+            if (PermissionsAPI.hasPermission(senderName, "ess.break")) {
+                val maxDistance = 20
+                for (i in 1..maxDistance) {
+                    val eyePos = senderPlayer.getEyePosition(1.0f)
+                    val lookVec = senderPlayer.getLook(1.0f)
+                    val targetObjectPos = eyePos.add(
+                        (lookVec.x * i).roundToInt().toDouble(),
+                        (lookVec.y * i).roundToInt().toDouble(),
+                        (lookVec.z * i).roundToInt().toDouble()
+                    )
+                    val blockPos = BlockPos(targetObjectPos.x, targetObjectPos.y, targetObjectPos.z)
+                    val blockState = senderPlayer.world.getBlockState(blockPos)
+                    val blockMaterial = blockState.material
+                    if (blockMaterial != Material.AIR) {
+                        when {
+                            config.restrictedBlocks.contains(
+                                blockState.block.registryName.toString()
+                            ) && !PermissionsAPI.hasPermission(
+                                senderName, "break.restricted.bypass"
+                            ) -> sendMsg(sender, "break.restricted_block")
+                            else -> sender.world.destroyBlock(blockPos, false)
+                        }
+                        break
+                    }
                 }
-                break
+            } else {
+                logger.warn(
+                    PERMISSION_LEVEL
+                        .replace("%0", senderName)
+                        .replace("%1", command)
+                )
+                sendMsg(sender, "break.restricted", targetName)
             }
         }
 
-        logger.info("Executed command \"/$commandName\" from $senderNickName")
-        return true
+        logger.info("Executed command \"/$command\" from $senderName")
+        return 0
     }
 }
