7744752b972774f1ede6086dc8f184b719b39381
Pavel Erokhin <MairwunNx@gmail.com>
2019-10-09 23:49:43 +0300

    MoreCommand class code refactoring.

--- src/main/kotlin/com/mairwunnx/projectessentials/commands/general/MoreCommand.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/commands/general/MoreCommand.kt
@@ -1,36 +1,39 @@
 package com.mairwunnx.projectessentials.commands.general
 
 import com.mairwunnx.projectessentials.commands.CommandBase
-import com.mairwunnx.projectessentials.configurations.CommandsConfig
 import com.mairwunnx.projectessentials.configurations.ModConfiguration.getCommandsConfig
 import com.mairwunnx.projectessentials.extensions.sendMsg
+import com.mairwunnx.projectessentialscore.helpers.ONLY_PLAYER_CAN
+import com.mairwunnx.projectessentialscore.helpers.PERMISSION_LEVEL
+import com.mairwunnx.projectessentialspermissions.permissions.PermissionsAPI
 import com.mojang.brigadier.CommandDispatcher
 import com.mojang.brigadier.builder.LiteralArgumentBuilder.literal
 import com.mojang.brigadier.context.CommandContext
-import kotlinx.serialization.UnstableDefault
 import net.minecraft.command.CommandSource
 import net.minecraft.util.Hand
 import org.apache.logging.log4j.LogManager
 
-@UnstableDefault
-object MoreCommand : CommandBase<CommandsConfig.Commands.More>(
-    getCommandsConfig().commands.more,
-    hasArguments = false
-) {
+object MoreCommand : CommandBase() {
     private val logger = LogManager.getLogger()
+    private var config = getCommandsConfig().commands.more
+
+    init {
+        command = "more"
+        aliases = config.aliases.toMutableList()
+    }
 
     override fun reload() {
-        commandInstance = getCommandsConfig().commands.more
+        config = getCommandsConfig().commands.more
+        aliases = config.aliases.toMutableList()
         super.reload()
     }
 
     override fun register(dispatcher: CommandDispatcher<CommandSource>) {
         super.register(dispatcher)
-        commandAliases.forEach { command ->
+        aliases.forEach { command ->
             dispatcher.register(literal<CommandSource>(command)
                 .executes {
-                    execute(it)
-                    return@executes 0
+                    return@executes execute(it)
                 }
             )
         }
@@ -38,20 +41,31 @@ object MoreCommand : CommandBase<CommandsConfig.Commands.More>(
 
     override fun execute(
         c: CommandContext<CommandSource>,
-        hasTarget: Boolean
-    ): Boolean {
-        val code = super.execute(c, hasTarget)
-        if (!code) return false
+        argument: Any?
+    ): Int {
+        super.execute(c, argument)
 
-        val item = senderPlayer.getHeldItem(Hand.MAIN_HAND)
-        if (item.count < item.maxStackSize) {
-            item.count = item.maxStackSize
-            sendMsg(sender, "more.out")
+        if (senderIsServer) {
+            logger.warn(ONLY_PLAYER_CAN.replace("%0", command))
         } else {
-            sendMsg(sender, "more.fullstack")
+            if (PermissionsAPI.hasPermission(senderName, "ess.more")) {
+                val item = senderPlayer.getHeldItem(Hand.MAIN_HAND)
+                if (item.count < item.maxStackSize) {
+                    item.count = item.maxStackSize
+                    sendMsg(sender, "more.out")
+                } else {
+                    sendMsg(sender, "more.fullstack")
+                }
+            } else {
+                logger.warn(
+                    PERMISSION_LEVEL
+                        .replace("%0", senderName)
+                        .replace("%1", command)
+                )
+                sendMsg(sender, "more.restricted", targetName)
+            }
         }
-
-        logger.info("Executed command \"/$commandName\" from $senderNickName")
-        return true
+        logger.info("Executed command \"/$command\" from $senderName")
+        return 0
     }
 }
