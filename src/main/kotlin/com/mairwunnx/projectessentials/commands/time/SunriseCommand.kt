2ac4eb4e5ff11319431c3e3c23272f02b52479eb
Pavel Erokhin <MairwunNx@gmail.com>
2019-10-10 19:58:30 +0300

    SunriseCommand class code refactoring.

--- src/main/kotlin/com/mairwunnx/projectessentials/commands/time/SunriseCommand.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/commands/time/SunriseCommand.kt
@@ -1,51 +1,20 @@
 package com.mairwunnx.projectessentials.commands.time
 
-import com.mairwunnx.projectessentials.commands.CommandBase
-import com.mairwunnx.projectessentials.configurations.CommandsConfig
+import com.mairwunnx.projectessentials.commands.CommandTimeBase
 import com.mairwunnx.projectessentials.configurations.ModConfiguration.getCommandsConfig
-import com.mairwunnx.projectessentials.extensions.sendMsg
-import com.mojang.brigadier.CommandDispatcher
-import com.mojang.brigadier.builder.LiteralArgumentBuilder.literal
-import com.mojang.brigadier.context.CommandContext
-import kotlinx.serialization.UnstableDefault
-import net.minecraft.command.CommandSource
-import org.apache.logging.log4j.LogManager
 
-@UnstableDefault
-object SunriseCommand : CommandBase<CommandsConfig.Commands.Sunrise>(
-    getCommandsConfig().commands.sunrise,
-    hasArguments = false
-) {
-    private val logger = LogManager.getLogger()
+object SunriseCommand : CommandTimeBase() {
+    private var config = getCommandsConfig().commands.sunrise
+
+    init {
+        command = "sunrise"
+        time = 23000
+        aliases = config.aliases.toMutableList()
+    }
 
     override fun reload() {
-        commandInstance = getCommandsConfig().commands.sunrise
+        config = getCommandsConfig().commands.sunrise
+        aliases = config.aliases.toMutableList()
         super.reload()
-    }
-
-    override fun register(dispatcher: CommandDispatcher<CommandSource>) {
-        super.register(dispatcher)
-        commandAliases.forEach { command ->
-            dispatcher.register(literal<CommandSource>(command)
-                .executes {
-                    execute(it)
-                    return@executes 0
-                }
-            )
-        }
-    }
-
-    override fun execute(
-        c: CommandContext<CommandSource>,
-        hasTarget: Boolean
-    ): Boolean {
-        val code = super.execute(c, hasTarget)
-        if (!code) return false
-
-        sender.world.dayTime = 23000
-        sendMsg(sender, "sunrise.installed")
-
-        logger.info("Executed command \"/$commandName\" from $senderNickName")
-        return true
     }
 }
