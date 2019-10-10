240c3462775323eb49261b24e1a905b3e4b5c19f
Pavel Erokhin <MairwunNx@gmail.com>
2019-10-10 19:45:15 +0300

    DayCommand class code refactoring. Also added CommandTimeBase abstract class for time based commands.

---
+++ src/main/kotlin/com/mairwunnx/projectessentials/commands/CommandTimeBase.kt
@@ -0,0 +1,58 @@
+package com.mairwunnx.projectessentials.commands
+
+import com.mairwunnx.projectessentials.extensions.sendMsg
+import com.mairwunnx.projectessentialscore.helpers.ONLY_PLAYER_CAN
+import com.mairwunnx.projectessentialscore.helpers.PERMISSION_LEVEL
+import com.mairwunnx.projectessentialspermissions.permissions.PermissionsAPI
+import com.mojang.brigadier.CommandDispatcher
+import com.mojang.brigadier.builder.LiteralArgumentBuilder.literal
+import com.mojang.brigadier.context.CommandContext
+import net.minecraft.command.CommandSource
+import org.apache.logging.log4j.LogManager
+
+open class CommandTimeBase : CommandBase() {
+    private val logger = LogManager.getLogger()
+    var time = 0L
+
+    override fun register(dispatcher: CommandDispatcher<CommandSource>) {
+        super.register(dispatcher)
+        aliases.forEach { command ->
+            dispatcher.register(literal<CommandSource>(command)
+                .executes {
+                    return@executes execute(it)
+                }
+            )
+        }
+    }
+
+    override fun execute(
+        c: CommandContext<CommandSource>,
+        argument: Any?
+    ): Int {
+        super.execute(c, argument)
+        if (senderIsServer) {
+            logger.warn(ONLY_PLAYER_CAN.replace("%0", command))
+            return 0
+        } else {
+            if (hasPermission()) {
+                sender.world.dayTime = time
+                sendMsg(sender, "$command.installed")
+            } else {
+                logger.warn(
+                    PERMISSION_LEVEL
+                        .replace("%0", senderName)
+                        .replace("%1", command)
+                )
+                sendMsg(sender, "$command.restricted", senderName)
+                return 0
+            }
+        }
+        logger.info("Executed command \"/$command\" from $senderName")
+        return 0
+    }
+
+    fun hasPermission(): Boolean {
+        return (PermissionsAPI.hasPermission(senderName, "ess.$command") ||
+                PermissionsAPI.hasPermission(senderName, "ess.time"))
+    }
+}
--- src/main/kotlin/com/mairwunnx/projectessentials/commands/time/DayCommand.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/commands/time/DayCommand.kt
@@ -1,23 +1,14 @@
 package com.mairwunnx.projectessentials.commands.time
 
-import com.mairwunnx.projectessentials.commands.CommandBase
+import com.mairwunnx.projectessentials.commands.CommandTimeBase
 import com.mairwunnx.projectessentials.configurations.ModConfiguration.getCommandsConfig
-import com.mairwunnx.projectessentials.extensions.sendMsg
-import com.mairwunnx.projectessentialscore.helpers.ONLY_PLAYER_CAN
-import com.mairwunnx.projectessentialscore.helpers.PERMISSION_LEVEL
-import com.mairwunnx.projectessentialspermissions.permissions.PermissionsAPI
-import com.mojang.brigadier.CommandDispatcher
-import com.mojang.brigadier.builder.LiteralArgumentBuilder.literal
-import com.mojang.brigadier.context.CommandContext
-import net.minecraft.command.CommandSource
-import org.apache.logging.log4j.LogManager
 
-object DayCommand : CommandBase() {
-    private val logger = LogManager.getLogger()
+object DayCommand : CommandTimeBase() {
     private var config = getCommandsConfig().commands.day
 
     init {
         command = "day"
+        time = 1000
         aliases = config.aliases.toMutableList()
     }
 
@@ -25,43 +16,5 @@ object DayCommand : CommandBase() {
         config = getCommandsConfig().commands.day
         aliases = config.aliases.toMutableList()
         super.reload()
-    }
-
-    override fun register(dispatcher: CommandDispatcher<CommandSource>) {
-        super.register(dispatcher)
-        aliases.forEach { command ->
-            dispatcher.register(literal<CommandSource>(command)
-                .executes {
-                    return@executes execute(it)
-                }
-            )
-        }
-    }
-
-    override fun execute(
-        c: CommandContext<CommandSource>,
-        argument: Any?
-    ): Int {
-        super.execute(c, argument)
-
-        if (senderIsServer) {
-            logger.warn(ONLY_PLAYER_CAN.replace("%0", command))
-            return 0
-        } else {
-            if (PermissionsAPI.hasPermission(senderName, "ess.day")) {
-                sender.world.dayTime = 1000
-                sendMsg(sender, "day.installed")
-            } else {
-                logger.warn(
-                    PERMISSION_LEVEL
-                        .replace("%0", senderName)
-                        .replace("%1", command)
-                )
-                sendMsg(sender, "day.restricted", senderName)
-                return 0
-            }
-        }
-        logger.info("Executed command \"/$command\" from $senderName")
-        return 0
     }
 }
