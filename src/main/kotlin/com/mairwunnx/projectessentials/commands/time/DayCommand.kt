83f5760c107cca6dbe1a6ab6290fd3397bf12262
Pavel Erokhin <MairwunNx@gmail.com>
2019-10-10 19:25:17 +0300

    DayCommand class code refactoring.

--- src/main/kotlin/com/mairwunnx/projectessentials/commands/time/DayCommand.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/commands/time/DayCommand.kt
@@ -1,35 +1,38 @@
 package com.mairwunnx.projectessentials.commands.time
 
 import com.mairwunnx.projectessentials.commands.CommandBase
-import com.mairwunnx.projectessentials.configurations.CommandsConfig
 import com.mairwunnx.projectessentials.configurations.ModConfiguration.getCommandsConfig
 import com.mairwunnx.projectessentials.extensions.sendMsg
+import com.mairwunnx.projectessentialscore.helpers.ONLY_PLAYER_CAN
+import com.mairwunnx.projectessentialscore.helpers.PERMISSION_LEVEL
+import com.mairwunnx.projectessentialspermissions.permissions.PermissionsAPI
 import com.mojang.brigadier.CommandDispatcher
 import com.mojang.brigadier.builder.LiteralArgumentBuilder.literal
 import com.mojang.brigadier.context.CommandContext
-import kotlinx.serialization.UnstableDefault
 import net.minecraft.command.CommandSource
 import org.apache.logging.log4j.LogManager
 
-@UnstableDefault
-object DayCommand : CommandBase<CommandsConfig.Commands.Day>(
-    getCommandsConfig().commands.day,
-    hasArguments = false
-) {
+object DayCommand : CommandBase() {
     private val logger = LogManager.getLogger()
+    private var config = getCommandsConfig().commands.day
+
+    init {
+        command = "day"
+        aliases = config.aliases.toMutableList()
+    }
 
     override fun reload() {
-        commandInstance = getCommandsConfig().commands.day
+        config = getCommandsConfig().commands.day
+        aliases = config.aliases.toMutableList()
         super.reload()
     }
 
     override fun register(dispatcher: CommandDispatcher<CommandSource>) {
         super.register(dispatcher)
-        commandAliases.forEach { command ->
+        aliases.forEach { command ->
             dispatcher.register(literal<CommandSource>(command)
                 .executes {
-                    execute(it)
-                    return@executes 0
+                    return@executes execute(it)
                 }
             )
         }
@@ -37,15 +40,28 @@ object DayCommand : CommandBase<CommandsConfig.Commands.Day>(
 
     override fun execute(
         c: CommandContext<CommandSource>,
-        hasTarget: Boolean
-    ): Boolean {
-        val code = super.execute(c, hasTarget)
-        if (!code) return false
+        argument: Any?
+    ): Int {
+        super.execute(c, argument)
 
-        sender.world.dayTime = 1000
-        sendMsg(sender, "day.installed")
-
-        logger.info("Executed command \"/$commandName\" from $senderNickName")
-        return true
+        if (senderIsServer) {
+            logger.warn(ONLY_PLAYER_CAN.replace("%0", command))
+            return 0
+        } else {
+            if (PermissionsAPI.hasPermission(senderName, "ess.day")) {
+                sender.world.dayTime = 1000
+                sendMsg(sender, "day.installed")
+            } else {
+                logger.warn(
+                    PERMISSION_LEVEL
+                        .replace("%0", senderName)
+                        .replace("%1", command)
+                )
+                sendMsg(sender, "day.restricted", senderName)
+                return 0
+            }
+        }
+        logger.info("Executed command \"/$command\" from $senderName")
+        return 0
     }
 }
