bc743e65da399e927f1275c2179e02bfe425ca9d
Pavel Erokhin <MairwunNx@gmail.com>
2019-10-10 20:07:27 +0300

    TimeCommand class code refactoring.

--- src/main/kotlin/com/mairwunnx/projectessentials/commands/time/TimeCommand.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/commands/time/TimeCommand.kt
@@ -1,37 +1,39 @@
 package com.mairwunnx.projectessentials.commands.time
 
 import com.mairwunnx.projectessentials.commands.CommandBase
-import com.mairwunnx.projectessentials.configurations.CommandsConfig
 import com.mairwunnx.projectessentials.configurations.ModConfiguration.getCommandsConfig
 import com.mairwunnx.projectessentials.extensions.sendMsg
 import com.mairwunnx.projectessentials.helpers.get24TimeFromTicks
+import com.mairwunnx.projectessentialscore.helpers.ONLY_PLAYER_CAN
+import com.mairwunnx.projectessentialscore.helpers.PERMISSION_LEVEL
+import com.mairwunnx.projectessentialspermissions.permissions.PermissionsAPI
 import com.mojang.brigadier.CommandDispatcher
 import com.mojang.brigadier.builder.LiteralArgumentBuilder.literal
 import com.mojang.brigadier.context.CommandContext
-import kotlinx.serialization.UnstableDefault
 import net.minecraft.command.CommandSource
 import org.apache.logging.log4j.LogManager
 
-@UnstableDefault
-object TimeCommand : CommandBase<CommandsConfig.Commands.Time>(
-    getCommandsConfig().commands.time,
-    hasArguments = false,
-    serverCanExecute = false
-) {
+object TimeCommand : CommandBase() {
     private val logger = LogManager.getLogger()
+    private var config = getCommandsConfig().commands.time
+
+    init {
+        command = "time"
+        aliases = config.aliases.toMutableList()
+    }
 
     override fun reload() {
-        commandInstance = getCommandsConfig().commands.time
+        config = getCommandsConfig().commands.time
+        aliases = config.aliases.toMutableList()
         super.reload()
     }
 
     override fun register(dispatcher: CommandDispatcher<CommandSource>) {
         super.register(dispatcher)
-        commandAliases.forEach { command ->
+        aliases.forEach { command ->
             dispatcher.register(literal<CommandSource>(command)
                 .executes {
-                    execute(it)
-                    return@executes 0
+                    return@executes execute(it)
                 }
             )
         }
@@ -39,21 +41,31 @@ object TimeCommand : CommandBase<CommandsConfig.Commands.Time>(
 
     override fun execute(
         c: CommandContext<CommandSource>,
-        hasTarget: Boolean
-    ): Boolean {
-        val code = super.execute(c, hasTarget)
-        if (!code) return false
-        val commandName = c.input.drop(1)
+        argument: Any?
+    ): Int {
+        super.execute(c, argument)
 
-        if (commandName == "time") {
-            sendMsg(
-                sender,
-                "time.out",
-                get24TimeFromTicks(sender.world.worldInfo.dayTime)
-            )
+        if (senderIsServer) {
+            logger.warn(ONLY_PLAYER_CAN.replace("%0", command))
+            return 0
+        } else {
+            if (PermissionsAPI.hasPermission(senderName, "ess.time")) {
+                sendMsg(
+                    sender,
+                    "time.out",
+                    get24TimeFromTicks(sender.world.worldInfo.dayTime)
+                )
+            } else {
+                logger.warn(
+                    PERMISSION_LEVEL
+                        .replace("%0", senderName)
+                        .replace("%1", command)
+                )
+                sendMsg(sender, "time.see.restricted", senderName)
+                return 0
+            }
         }
-
-        logger.info("Executed command \"/$commandName\" from $senderNickName")
-        return true
+        logger.info("Executed command \"/$command\" from $senderName")
+        return 0
     }
 }
