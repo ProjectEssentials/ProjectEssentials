33e69eb6d629527fb67b9f62c3724080f86f89eb
Pavel Erokhin <MairwunNx@gmail.com>
2019-10-12 00:02:42 +0300

    Fixed CommandAliases error for searching blocked command.

---
+++ src/main/kotlin/com/mairwunnx/projectessentials/commands/helpers/CommandAliases.kt
@@ -0,0 +1,22 @@
+package com.mairwunnx.projectessentials.commands.helpers
+
+import com.mairwunnx.projectessentials.configurations.ModConfiguration
+import com.mairwunnx.projectessentialscooldown.essentials.CommandsAliases.aliases
+
+object CommandAliases {
+    /**
+     * Return true if command argument is alias of
+     * blocked command.
+     */
+    fun searchForAliases(command: String): Boolean {
+        val modConfig = ModConfiguration.getCommandsConfig()
+        aliases.keys.forEach { baseCommand ->
+            val aliasesOfCommands = aliases[baseCommand]
+            if (aliasesOfCommands != null &&
+                aliasesOfCommands.contains(command) &&
+                modConfig.disabledCommands.contains(baseCommand)
+            ) return true
+        }
+        return false
+    }
+}
