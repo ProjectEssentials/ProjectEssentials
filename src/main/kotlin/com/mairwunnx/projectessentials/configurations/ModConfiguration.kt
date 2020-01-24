6093bccf60b4f209da4963fbd4946bf78bf1b408
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-01-24 20:58:51 +0300

    ModConfiguration.kt code refactoring.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/configurations/ModConfiguration.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/configurations/ModConfiguration.kt
@@ -2,70 +2,33 @@ package com.mairwunnx.projectessentials.configurations
 
 import com.mairwunnx.projectessentials.COMMANDS_CONFIG
 import com.mairwunnx.projectessentials.core.helpers.MOD_CONFIG_FOLDER
-import kotlinx.serialization.UnstableDefault
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonConfiguration
+import com.mairwunnx.projectessentials.core.helpers.jsonInstance
 import org.apache.logging.log4j.LogManager
 import java.io.File
 import java.io.FileNotFoundException
 
-@UseExperimental(UnstableDefault::class)
 object ModConfiguration {
     private val logger = LogManager.getLogger()
     private var commandsConfig = CommandsConfig()
 
     fun loadConfig() {
-        logger.info("    - loading base modification configuration ...")
-        loadCommandsConfig()
-    }
+        logger.info("Loading commands configuration")
 
-    private fun loadCommandsConfig() {
         try {
-            logger.info("        - loading commands configuration ...")
             val configRaw = File(COMMANDS_CONFIG).readText()
-            val json = Json(
-                JsonConfiguration(
-                    encodeDefaults = true,
-                    strictMode = false,
-                    unquoted = false,
-                    allowStructuredMapKeys = true,
-                    prettyPrint = true
-                )
-            )
-            commandsConfig = json.parse(CommandsConfig.serializer(), configRaw)
+            commandsConfig = jsonInstance.parse(CommandsConfig.serializer(), configRaw)
         } catch (ex: FileNotFoundException) {
             logger.error("Configuration file ($COMMANDS_CONFIG) not found!")
-            logger.warn("The default configuration will be used.")
+            logger.warn("The default configuration will be used")
         }
     }
 
     fun saveConfig() {
-        logger.debug("        - setup json configuration for parsing ...")
-        val json = Json(
-            JsonConfiguration(
-                encodeDefaults = true,
-                strictMode = false,
-                unquoted = false,
-                allowStructuredMapKeys = true,
-                prettyPrint = true,
-                useArrayPolymorphism = false
-            )
-        )
-        createConfigDirs()
-        saveCommandsConfig(json)
-    }
+        logger.info("Saving commands configuration")
+        File(MOD_CONFIG_FOLDER).mkdirs()
 
-    private fun createConfigDirs() {
-        logger.info("        - creating directories for configurations ...")
-        val configDirectory = File(MOD_CONFIG_FOLDER)
-        if (!configDirectory.exists()) configDirectory.mkdirs()
-    }
-
-    private fun saveCommandsConfig(json: Json) {
-        logger.info("        - saving commands configuration ...")
-        val commandsConfigRaw = json.stringify(
-            CommandsConfig.serializer(),
-            commandsConfig
+        val commandsConfigRaw = jsonInstance.stringify(
+            CommandsConfig.serializer(), commandsConfig
         )
 
         try {
