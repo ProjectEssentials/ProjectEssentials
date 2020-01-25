aec04b96a823c42bf98970b782bdaed46a5b1407
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-01-25 08:55:28 +0300

    Naming style for configuration fields changed.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/storage/UserData.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/storage/UserData.kt
@@ -1,17 +1,12 @@
 package com.mairwunnx.projectessentials.storage
 
 import com.mairwunnx.projectessentials.core.extensions.empty
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class UserData(
-    @SerialName("LastWorld")
     var lastWorld: String = String.empty,
-    @SerialName("LastWorldPos")
     var lastWorldPos: String = String.empty,
-    @SerialName("FlyEnabledWorlds")
     var flyEnabledInWorlds: List<String> = listOf(),
-    @SerialName("GodEnabledWorlds")
     var godEnabledWorlds: List<String> = listOf()
 )
