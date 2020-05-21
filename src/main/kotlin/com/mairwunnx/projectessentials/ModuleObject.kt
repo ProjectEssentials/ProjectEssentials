5202204a9a08bb7c18f3db4309906ef2417f692f
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-05-21 15:49:37 +0300

    Initializing module settings added.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/ModuleObject.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/ModuleObject.kt
@@ -2,12 +2,15 @@
 
 package com.mairwunnx.projectessentials
 
+import com.mairwunnx.projectessentials.core.api.v1.configuration.ConfigurationAPI.getConfigurationByName
 import com.mairwunnx.projectessentials.core.api.v1.events.ModuleEventAPI
 import com.mairwunnx.projectessentials.core.api.v1.events.forge.FMLCommonSetupEventData
 import com.mairwunnx.projectessentials.core.api.v1.events.forge.ForgeEventType
 import com.mairwunnx.projectessentials.core.api.v1.localization.Localization
 import com.mairwunnx.projectessentials.core.api.v1.localization.LocalizationAPI
 import com.mairwunnx.projectessentials.core.api.v1.module.IModule
+import com.mairwunnx.projectessentials.core.impl.commands.ConfigureEssentialsCommandAPI
+import com.mairwunnx.projectessentials.core.impl.configurations.GeneralConfiguration
 import net.minecraftforge.common.MinecraftForge.EVENT_BUS
 import net.minecraftforge.fml.common.Mod
 import org.apache.logging.log4j.LogManager
@@ -18,6 +21,10 @@ class ModuleObject : IModule {
     override val version = this::class.java.`package`.implementationVersion!!
     override val loadIndex = 2
 
+    private val generalConfiguration by lazy {
+        getConfigurationByName<GeneralConfiguration>("general")
+    }
+
     private val logger = LogManager.getLogger()
 
     init {
@@ -25,7 +32,24 @@ class ModuleObject : IModule {
         EVENT_BUS.register(this)
     }
 
-    override fun init() = Unit
+    override fun init() = initializeModuleSettings()
+
+    private fun initializeModuleSettings() {
+        generalConfiguration.getFloatOrDefault(SETTING_MAX_FOOD_SATURATION_LEVEL, 5.0f)
+        generalConfiguration.getIntOrDefault(SETTING_MAX_FOOD_LEVEL, 20)
+        generalConfiguration.getBoolOrDefault(SETTING_AUTO_FLY_MODE_ON_JOIN_ENABLED, true)
+        generalConfiguration.getBoolOrDefault(SETTING_AUTO_GOD_MODE_ON_JOIN_ENABLED, true)
+        generalConfiguration.getIntOrDefault(SETTING_BURN_COMMAND_DURATION, 10)
+        generalConfiguration.getIntOrDefault(SETTING_TELEPORT_REQUEST_TIMEOUT, 45)
+        generalConfiguration.getBoolOrDefault(SETTING_REPLACE_NATIVE_HELP_COMMAND, true)
+        generalConfiguration.getIntOrDefault(SETTING_HELP_COMMAND_MAX_LINES_OUT, 8)
+        generalConfiguration.getBoolOrDefault(SETTING_HELP_COMMAND_COLORIZED_OUT, true)
+        generalConfiguration.getList(SETTING_FIRST_JOIN_COMMANDS, arrayListOf())
+        generalConfiguration.getIntOrDefault(SETTING_AFK_IDLENESS_TIME, 300)
+        generalConfiguration.getBoolOrDefault(SETTING_AFK_HANDLE_ACTIVITY, true)
+
+        ConfigureEssentialsCommandAPI.required(SETTING_REPLACE_NATIVE_HELP_COMMAND)
+    }
 
     private fun subscribeEvents() {
         ModuleEventAPI.subscribeOn<FMLCommonSetupEventData>(
