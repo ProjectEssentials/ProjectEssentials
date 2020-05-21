bb3b230863d1368089163ca1012e63234cc119fb
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-05-21 15:48:57 +0300

    Settings constants added to `InternalConstants.kt`.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/InternalConstants.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/InternalConstants.kt
@@ -1,2 +1,14 @@
 package com.mairwunnx.projectessentials
 
+const val SETTING_MAX_FOOD_SATURATION_LEVEL = "feed-max-food-saturation-level"
+const val SETTING_MAX_FOOD_LEVEL = "feed-max-food-level"
+const val SETTING_AUTO_FLY_MODE_ON_JOIN_ENABLED = "auto-fly-mode-on-join-enabled"
+const val SETTING_AUTO_GOD_MODE_ON_JOIN_ENABLED = "auto-god-mode-on-join-enabled"
+const val SETTING_BURN_COMMAND_DURATION = "burn-command-duration"
+const val SETTING_TELEPORT_REQUEST_TIMEOUT = "teleport-request-timeout"
+const val SETTING_REPLACE_NATIVE_HELP_COMMAND = "replace-native-help-command"
+const val SETTING_HELP_COMMAND_MAX_LINES_OUT = "help-command-max-lines-out"
+const val SETTING_HELP_COMMAND_COLORIZED_OUT = "help-command-colorized-out"
+const val SETTING_FIRST_JOIN_COMMANDS = "first-join-commands"
+const val SETTING_AFK_IDLENESS_TIME = "afk-idleness-time"
+const val SETTING_AFK_HANDLE_ACTIVITY = "afk-handle-activity"
