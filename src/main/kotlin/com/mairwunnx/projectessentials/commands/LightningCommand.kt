c68166f6138885ddd7929a00e54471b50a7ca7a3
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-05-26 22:15:32 +0300

    Alias `thor` added for lightning command.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/commands/LightningCommand.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/commands/LightningCommand.kt
@@ -14,6 +14,7 @@ import net.minecraft.world.server.ServerWorld
 
 object LightningCommand : CommandBase(helpLiteral) {
     override val name = "lightning"
+    override val aliases = listOf("thor")
 
     override fun process(context: CommandContext<CommandSource>) = 0.also {
         validateAndExecute(context, "ess.lightning", 3) { isServer ->
