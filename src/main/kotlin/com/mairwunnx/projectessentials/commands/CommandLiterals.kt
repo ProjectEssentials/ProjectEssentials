43903493d5405e1cbbf45cc46457effa231aca31
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-05-27 00:08:42 +0300

    CommandLiterals.kt formatted.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/commands/CommandLiterals.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/commands/CommandLiterals.kt
@@ -15,12 +15,14 @@ val afkLiteral: LiteralArgumentBuilder<CommandSource> =
             ).executes { AfkCommand.afkList(it) }
         ).executes { AfkCommand.afkList(it) }
     ).executes { AfkCommand.afkSet(it) }
+
 val airLiteral: LiteralArgumentBuilder<CommandSource> =
     literal<CommandSource>("air").then(
         Commands.argument(
             "targets", EntityArgument.players()
         ).executes { AirCommand.airOther(it) }
     ).executes { AirCommand.airSelf(it) }
+
 val burnLiteral: LiteralArgumentBuilder<CommandSource> =
     literal<CommandSource>("burn").then(
         Commands.argument(
@@ -31,48 +33,57 @@ val burnLiteral: LiteralArgumentBuilder<CommandSource> =
             ).executes { BurnCommand.burnOther(it) }
         ).executes { BurnCommand.burnSelf(it) }
     )
+
 val feedLiteral: LiteralArgumentBuilder<CommandSource> =
     literal<CommandSource>("feed").then(
         Commands.argument(
             "targets", EntityArgument.players()
         ).executes { FeedCommand.feedOther(it) }
     ).executes { FeedCommand.feedSelf(it) }
+
 val healLiteral: LiteralArgumentBuilder<CommandSource> =
     literal<CommandSource>("heal").then(
         Commands.argument(
             "targets", EntityArgument.players()
         ).executes { HealCommand.healOther(it) }
     ).executes { HealCommand.healSelf(it) }
+
 val flyLiteral: LiteralArgumentBuilder<CommandSource> =
     literal<CommandSource>("fly").then(
         Commands.argument(
             "targets", EntityArgument.players()
         ).executes { FlyCommand.flyOther(it) }
     ).executes { FlyCommand.flySelf(it) }
+
 val godLiteral: LiteralArgumentBuilder<CommandSource> =
     literal<CommandSource>("god").then(
         Commands.argument(
             "targets", EntityArgument.players()
         ).executes { GodCommand.godOther(it) }
     ).executes { GodCommand.godSelf(it) }
+
 val helpLiteral: LiteralArgumentBuilder<CommandSource> =
     literal<CommandSource>("help").then(
         Commands.argument(
             "page", IntegerArgumentType.integer()
         ).executes { HelpCommand.process(it) }
     ).executes { HelpCommand.process(it) }
+
 val lightningLiteral: LiteralArgumentBuilder<CommandSource> =
     literal<CommandSource>("lightning").then(
         Commands.argument(
             "targets", EntityArgument.entities()
         ).executes { LightningCommand.process(it) }
     )
+
 val moreLiteral: LiteralArgumentBuilder<CommandSource> = literal("more")
 val pingLiteral: LiteralArgumentBuilder<CommandSource> = literal("ping")
+
 val repairLiteral: LiteralArgumentBuilder<CommandSource> =
     literal<CommandSource>("repair").then(
         Commands.literal("all").executes { RepairCommand.repairAll(it) }
     ).executes { RepairCommand.repair(it) }
+
 val sendPosLiteral: LiteralArgumentBuilder<CommandSource> =
     literal<CommandSource>("sendpos").then(
         Commands.argument(
