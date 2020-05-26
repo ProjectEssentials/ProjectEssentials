aaa65935626798aa36c9b1810a8ca2c46a3e47a5
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-05-26 17:51:02 +0300

    Feed command re-written.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/commands/FeedCommand.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/commands/FeedCommand.kt
@@ -1,164 +1,101 @@
 package com.mairwunnx.projectessentials.commands
 
-import com.mairwunnx.projectessentials.configurations.ModConfiguration.getCommandsConfig
-import com.mairwunnx.projectessentials.core.helpers.DISABLED_COMMAND_ARG
-import com.mairwunnx.projectessentials.core.helpers.throwOnlyPlayerCan
-import com.mairwunnx.projectessentials.core.helpers.throwPermissionLevel
-import com.mairwunnx.projectessentials.extensions.isNeedFood
-import com.mairwunnx.projectessentials.extensions.sendMsg
-import com.mairwunnx.projectessentials.permissions.permissions.PermissionsAPI
-import com.mojang.brigadier.CommandDispatcher
-import com.mojang.brigadier.builder.LiteralArgumentBuilder.literal
+import com.mairwunnx.projectessentials.core.api.v1.MESSAGE_MODULE_PREFIX
+import com.mairwunnx.projectessentials.core.api.v1.commands.CommandAPI
+import com.mairwunnx.projectessentials.core.api.v1.commands.CommandBase
+import com.mairwunnx.projectessentials.core.api.v1.extensions.getPlayer
+import com.mairwunnx.projectessentials.core.api.v1.extensions.playerName
+import com.mairwunnx.projectessentials.core.api.v1.messaging.MessagingAPI
+import com.mairwunnx.projectessentials.core.api.v1.messaging.ServerMessagingAPI
+import com.mairwunnx.projectessentials.isNeedFood
+import com.mairwunnx.projectessentials.validateAndExecute
 import com.mojang.brigadier.context.CommandContext
 import net.minecraft.command.CommandSource
-import net.minecraft.command.Commands
-import net.minecraft.command.arguments.EntityArgument
 import net.minecraft.entity.player.ServerPlayerEntity
 import net.minecraft.util.FoodStats
 import net.minecraftforge.api.distmarker.Dist
 import net.minecraftforge.fml.DistExecutor
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper
-import org.apache.logging.log4j.LogManager
 
-object FeedCommand : CommandBase() {
-    private val logger = LogManager.getLogger()
-    private var maxSaturateLevel = 5.0f
-    private var maxFoodLevel = 20
+object FeedCommand : CommandBase(feedLiteral, false) {
+    override val name = "feed"
+    override val aliases = listOf("eat")
+
+    private const val maxSaturateLevel = 5.0f
+    private const val maxFoodLevel = 20
+
     private val saturationLevel by lazy {
-        return@lazy ObfuscationReflectionHelper.findField(
-            FoodStats::class.java,
-            "field_75125_b"
+        ObfuscationReflectionHelper.findField(
+            FoodStats::class.java, "field_75125_b"
         )
     }
-    private var config = getCommandsConfig().commands.feed
 
-    init {
-        command = "feed"
-        aliases = config.aliases.toMutableList()
-    }
-
-    override fun reload() {
-        config = getCommandsConfig().commands.feed
-        aliases = config.aliases.toMutableList()
-        super.reload()
-    }
-
-    override fun register(dispatcher: CommandDispatcher<CommandSource>) {
-        super.register(dispatcher)
-        aliases.forEach { command ->
-            dispatcher.register(literal<CommandSource>(command)
-                .then(
-                    Commands.argument(
-                        "player", EntityArgument.player()
-                    ).executes {
-                        return@executes execute(it, true)
+    fun feedSelf(context: CommandContext<CommandSource>) = 0.also {
+        validateAndExecute(context, "ess.feed.self", 2) { isServer ->
+            if (isServer) {
+                ServerMessagingAPI.throwOnlyPlayerCan()
+            } else {
+                with(context.getPlayer()!!) {
+                    if (this.foodStats.isNeedFood()) {
+                        this.foodStats.foodLevel = maxFoodLevel
+                        saturateTarget(this)
                     }
-                )
-                .executes {
-                    return@executes execute(it)
-                }
-            )
+                    MessagingAPI.sendMessage(
+                        this, "${MESSAGE_MODULE_PREFIX}basic.feed.self.success"
+                    )
+                }.also { process(context) }
+            }
         }
     }
 
-    override fun execute(
-        c: CommandContext<CommandSource>,
-        argument: Any?
-    ): Int {
-        super.execute(c, argument)
-
-        maxFoodLevel = config.maxFoodLevel
-        maxSaturateLevel = config.maxFoodSaturationLevel
-
-        if (senderIsServer) {
-            if (targetIsExists) {
-                if (!targetPlayer.foodStats.isNeedFood()) {
-                    logger.info("Player $targetName appetite already fully sated.")
-                    return 0
+    fun feedOther(context: CommandContext<CommandSource>) = 0.also {
+        validateAndExecute(context, "ess.feed.other", 3) { isServer ->
+            val players = CommandAPI.getPlayers(context, "targets")
+            players.forEach { player ->
+                if (player.foodStats.isNeedFood()) {
+                    player.foodStats.foodLevel = maxFoodLevel
+                    saturateTarget(player)
                 }
-                logger.info(
-                    "Player ($targetName) food level/saturation changed from ${targetPlayer.foodStats.foodLevel}/${targetPlayer.foodStats.saturationLevel} to 20/5.0 by $senderName"
+                MessagingAPI.sendMessage(
+                    player,
+                    "${MESSAGE_MODULE_PREFIX}basic.feed.by.success",
+                    args = *arrayOf(context.playerName())
                 )
-                targetPlayer.foodStats.foodLevel = maxFoodLevel
-                saturateTarget(targetPlayer)
-                logger.info("You satiated the appetite of player $targetName.")
-                sendMsg(
-                    targetPlayer.commandSource,
-                    "feed.other.recipient_out",
-                    senderName
-                )
-            } else {
-                throwOnlyPlayerCan(command)
             }
-            return 0
-        } else {
-            if (targetIsExists) {
-                if (PermissionsAPI.hasPermission(senderName, "ess.feed.other")) {
-                    when {
-                        !config.enableArgs -> {
-                            logger.warn(
-                                DISABLED_COMMAND_ARG
-                                    .replace("%0", senderName)
-                                    .replace("%1", command)
-                            )
-                            sendMsg(sender, "common.arg.disabled", command)
-                            return 0
+            if (isServer) {
+                ServerMessagingAPI.response {
+                    if (players.count() == 1) {
+                        "You've saturated the ${players.first().name.string}'s appetite"
+                    } else {
+                        "You've saturated the selected (${players.count()}) players's appetite"
+                    }
+                }
+            } else {
+                MessagingAPI.sendMessage(
+                    context.getPlayer()!!,
+                    if (players.count() == 1) {
+                        "${MESSAGE_MODULE_PREFIX}basic.feed.other_single.success"
+                    } else {
+                        "${MESSAGE_MODULE_PREFIX}basic.feed.other_multiple.success"
+                    },
+                    args = *arrayOf(
+                        if (players.count() == 1) {
+                            players.first().name.string
+                        } else {
+                            players.count().toString()
                         }
-                    }
-
-                    if (!targetPlayer.foodStats.isNeedFood()) {
-                        sendMsg(sender, "feed.other.maxfeed", targetName)
-                        return 0
-                    }
-                    logger.info(
-                        "Player ($targetName) food level/saturation changed from ${targetPlayer.foodStats.foodLevel}/${targetPlayer.foodStats.saturationLevel} to 20/5.0 by $senderName"
                     )
-                    targetPlayer.foodStats.foodLevel = maxFoodLevel
-                    saturateTarget(targetPlayer)
-                    sendMsg(sender, "feed.other.success", targetName)
-                    sendMsg(
-                        target,
-                        "feed.other.recipient_out",
-                        senderName
-                    )
-                } else {
-                    throwPermissionLevel(senderName, command)
-                    sendMsg(sender, "feed.other.restricted", targetName)
-                    return 0
-                }
-            } else {
-                if (PermissionsAPI.hasPermission(senderName, "ess.feed")) {
-                    if (!senderPlayer.foodStats.isNeedFood()) {
-                        sendMsg(sender, "feed.self.maxfeed")
-                        return 0
-                    }
-                    logger.info(
-                        "Player ($senderName) food level/saturation changed from ${senderPlayer.foodStats.foodLevel}/${senderPlayer.foodStats.saturationLevel} to 20/5.0"
-                    )
-                    senderPlayer.foodStats.foodLevel = maxFoodLevel
-                    saturateTarget(senderPlayer)
-                    sendMsg(sender, "feed.self.success")
-                } else {
-                    throwPermissionLevel(senderName, command)
-                    sendMsg(sender, "feed.self.restricted", senderName)
-                    return 0
-                }
+                ).also { process(context) }
             }
         }
-        logger.info("Executed command \"/$command\" from $senderName")
-        return 0
     }
 
     private fun saturateTarget(target: ServerPlayerEntity) {
         DistExecutor.runWhenOn(Dist.CLIENT) {
-            Runnable {
-                target.foodStats.setFoodSaturationLevel(maxSaturateLevel)
-            }
+            Runnable { target.foodStats.setFoodSaturationLevel(maxSaturateLevel) }
         }
         DistExecutor.runWhenOn(Dist.DEDICATED_SERVER) {
-            Runnable {
-                saturationLevel.setFloat(target.foodStats, maxSaturateLevel)
-            }
+            Runnable { saturationLevel.setFloat(target.foodStats, maxSaturateLevel) }
         }
     }
 }
