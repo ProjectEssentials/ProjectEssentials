200c1e37a167b331a12c3608b48332eccb503892
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-05-26 17:24:19 +0300

    Burn command re-written.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/commands/BurnCommand.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/commands/BurnCommand.kt
@@ -1,78 +1,72 @@
 package com.mairwunnx.projectessentials.commands
 
-import com.mairwunnx.projectessentials.configurations.ModConfiguration.getCommandsConfig
-import com.mairwunnx.projectessentials.core.helpers.throwPermissionLevel
-import com.mairwunnx.projectessentials.extensions.sendMsg
-import com.mairwunnx.projectessentials.permissions.permissions.PermissionsAPI
-import com.mojang.brigadier.CommandDispatcher
-import com.mojang.brigadier.arguments.IntegerArgumentType
-import com.mojang.brigadier.builder.LiteralArgumentBuilder.literal
+import com.mairwunnx.projectessentials.core.api.v1.MESSAGE_MODULE_PREFIX
+import com.mairwunnx.projectessentials.core.api.v1.commands.CommandAPI
+import com.mairwunnx.projectessentials.core.api.v1.commands.CommandBase
+import com.mairwunnx.projectessentials.core.api.v1.extensions.getPlayer
+import com.mairwunnx.projectessentials.core.api.v1.extensions.playerName
+import com.mairwunnx.projectessentials.core.api.v1.messaging.MessagingAPI
+import com.mairwunnx.projectessentials.core.api.v1.messaging.ServerMessagingAPI
+import com.mairwunnx.projectessentials.validateAndExecute
 import com.mojang.brigadier.context.CommandContext
 import net.minecraft.command.CommandSource
-import net.minecraft.command.Commands
-import net.minecraft.command.arguments.EntityArgument
-import org.apache.logging.log4j.LogManager
 
-object BurnCommand : CommandBase() {
-    private val logger = LogManager.getLogger()
-    private var config = getCommandsConfig().commands.burn
+object BurnCommand : CommandBase(burnLiteral, false) {
+    override val name = "burn"
+    override val aliases = listOf("fire")
 
-    init {
-        command = "burn"
-        aliases = config.aliases.toMutableList()
-    }
-
-    override fun reload() {
-        config = getCommandsConfig().commands.burn
-        aliases = config.aliases.toMutableList()
-        super.reload()
-    }
-
-    override fun register(dispatcher: CommandDispatcher<CommandSource>) {
-        super.register(dispatcher)
-        aliases.forEach { command ->
-            dispatcher.register(literal<CommandSource>(command)
-                .then(
-                    Commands.argument("player", EntityArgument.player()).then(
-                        Commands.argument(
-                            "duration",
-                            IntegerArgumentType.integer(1, 120)
-                        ).executes {
-                            return@executes execute(
-                                it, IntegerArgumentType.getInteger(it, "duration")
-                            )
-                        }
-                    ).executes {
-                        return@executes execute(it, config.defaultDuration)
-                    }
-                )
-            )
-        }
-    }
-
-    override fun execute(
-        c: CommandContext<CommandSource>,
-        argument: Any?
-    ): Int {
-        super.execute(c, argument)
-
-        if (PermissionsAPI.hasPermission(senderName, "ess.burn", senderIsServer)) {
-            val target = EntityArgument.getPlayer(c, "player")
-            target.setFire(argument as Int)
-
-            if (senderIsServer) {
-                logger.info("You set ${target.name.string} on fire for $argument seconds.")
-                return 0
+    fun burnSelf(context: CommandContext<CommandSource>) = 0.also {
+        validateAndExecute(context, "ess.burn.self", 2) { isServer ->
+            if (isServer) {
+                ServerMessagingAPI.throwOnlyPlayerCan()
             } else {
-                sendMsg(sender, "burn.success", target.name.string, argument.toString())
+                with(context.getPlayer()!!) {
+                    this.setFire(CommandAPI.getInt(context, "duration"))
+                    MessagingAPI.sendMessage(
+                        this, "${MESSAGE_MODULE_PREFIX}basic.burn.self.success"
+                    )
+                }.also { process(context) }
             }
-        } else {
-            throwPermissionLevel(senderName, command)
-            sendMsg(sender, "burn.restricted", senderName)
-            return 0
         }
+    }
 
-        logger.info("Executed command \"/$command\" from $senderName")
-        return 0
+    fun burnOther(context: CommandContext<CommandSource>) = 0.also {
+        validateAndExecute(context, "ess.burn.other", 3) { isServer ->
+            val players = CommandAPI.getPlayers(context, "targets")
+            val duration = CommandAPI.getInt(context, "duration")
+            players.forEach { player ->
+                player.setFire(duration)
+                MessagingAPI.sendMessage(
+                    player,
+                    "${MESSAGE_MODULE_PREFIX}basic.burn.by.success",
+                    args = *arrayOf(context.playerName())
+                )
+            }
+            if (isServer) {
+                ServerMessagingAPI.response {
+                    if (players.count() == 1) {
+                        "You've burned the ${players.first().name.string} player for $duration seconds"
+                    } else {
+                        "You've burned the selected (${players.count()}) players for $duration seconds"
+                    }
+                }
+            } else {
+                MessagingAPI.sendMessage(
+                    context.getPlayer()!!,
+                    if (players.count() == 1) {
+                        "${MESSAGE_MODULE_PREFIX}basic.burn.other_single.success"
+                    } else {
+                        "${MESSAGE_MODULE_PREFIX}basic.burn.other_multiple.success"
+                    },
+                    args = *arrayOf(
+                        if (players.count() == 1) {
+                            players.first().name.string
+                        } else {
+                            players.count().toString()
+                        }
+                    )
+                ).also { process(context) }
+            }
+        }
     }
 }
