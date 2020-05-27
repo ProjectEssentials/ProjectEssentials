0814852f7957d83c851b941bda22b55ac42c0c5e
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-05-27 18:18:50 +0300

    Kits model done.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/configurations/KitsConfigurationModel.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/configurations/KitsConfigurationModel.kt
@@ -1,8 +1,54 @@
 package com.mairwunnx.projectessentials.configurations
 
+import com.mairwunnx.projectessentials.core.api.v1.extensions.empty
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class KitsConfigurationModel(
-    val kits: MutableList<String> = mutableListOf()
-)
+    var kits: MutableList<Kit> = mutableListOf(
+        Kit(
+            "newbies",
+            43200,
+            mutableListOf(
+                Kit.Item(
+                    "oak_wood", 8, "&7[&9Starter Item&7]", "%player's items from newbies kit"
+                ),
+                Kit.Item(
+                    "cooked_cod", 5, "&7[&9Starter Item&7]", "%player's items from newbies kit"
+                ),
+                Kit.Item(
+                    "cow_spawn_egg", 1, "&7[&9Starter Item&7]", "%player's items from newbies kit"
+                ),
+                Kit.Item(
+                    "stone", 16, "&7[&9Starter Item&7]", "%player's items from newbies kit"
+                ),
+                Kit.Item(
+                    "torch", 8, "&7[&9Starter Item&7]", "%player's items from newbies kit"
+                )
+            )
+        )
+    )
+) {
+    @Serializable
+    data class Kit(
+        var name: String,
+        var delay: Int = 0,
+        var items: MutableList<Item> = mutableListOf()
+
+    ) {
+        @Serializable
+        data class Item(
+            var name: String,
+            var count: Int = 1,
+            var displayName: String = String.empty,
+            var description: String = String.empty,
+            var enchantments: MutableList<Enchantment> = mutableListOf()
+        ) {
+            @Serializable
+            data class Enchantment(
+                var enchantment: String,
+                var level: Int = 1
+            )
+        }
+    }
+}
