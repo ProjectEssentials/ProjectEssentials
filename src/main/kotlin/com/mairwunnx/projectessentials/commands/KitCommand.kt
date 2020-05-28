9c9386279dd1585cc5c70a25980320d83ed9c5ca
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-05-28 13:33:34 +0300

    Kit get command implemented.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/commands/KitCommand.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/commands/KitCommand.kt
@@ -1,12 +1,18 @@
 package com.mairwunnx.projectessentials.commands
 
+import com.mairwunnx.projectessentials.core.api.v1.MESSAGE_MODULE_PREFIX
+import com.mairwunnx.projectessentials.core.api.v1.commands.CommandAPI
 import com.mairwunnx.projectessentials.core.api.v1.commands.CommandBase
+import com.mairwunnx.projectessentials.core.api.v1.extensions.getPlayer
+import com.mairwunnx.projectessentials.core.api.v1.extensions.isPlayerSender
 import com.mairwunnx.projectessentials.core.api.v1.messaging.MessagingAPI
 import com.mairwunnx.projectessentials.core.api.v1.messaging.ServerMessagingAPI
 import com.mairwunnx.projectessentials.managers.KitManager
+import com.mairwunnx.projectessentials.managers.KitManager.Response
 import com.mairwunnx.projectessentials.validateAndExecute
 import com.mojang.brigadier.context.CommandContext
 import net.minecraft.command.CommandSource
+import net.minecraft.entity.player.ServerPlayerEntity
 
 object KitCommand : CommandBase(kitLiteral, false) {
     override val name = "kit"
@@ -23,7 +29,24 @@ object KitCommand : CommandBase(kitLiteral, false) {
         }
     }
 
-    fun airSelf(context: CommandContext<CommandSource>) = 0.also {
+    fun kitGet(context: CommandContext<CommandSource>) = 0.also {
+        if (context.isPlayerSender()) {
+            val kit = CommandAPI.getString(context, "kit-name")
+            val player = context.getPlayer()!!
+            when (KitManager.requestKit(player, kit)) {
+                Response.Success -> kitSelfOut(player, kit, "success").also { process(context) }
+                Response.KitNoHasPermissions -> kitSelfOut(player, kit, "no_permission")
+                Response.KitNotFound -> kitSelfOut(player, kit, "not_found")
+                Response.KitTimeNotExpired -> kitSelfOut(player, kit, "kit_not_expired")
+            }
+        } else {
+            ServerMessagingAPI.throwOnlyPlayerCan()
+        }
+    }
 
+    private fun kitSelfOut(player: ServerPlayerEntity, kit: String, result: String) {
+        MessagingAPI.sendMessage(
+            player, "${MESSAGE_MODULE_PREFIX}basic.kit.self.$result", args = *arrayOf(kit)
+        )
     }
 }
