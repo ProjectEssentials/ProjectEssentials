8cdb8d4cbc49e089e60115264eeb349d551b7d96
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-05-28 13:23:45 +0300

    Kit list command added.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/commands/KitCommand.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/commands/KitCommand.kt
@@ -1,4 +1,29 @@
 package com.mairwunnx.projectessentials.commands
 
-object KitCommand {
+import com.mairwunnx.projectessentials.core.api.v1.commands.CommandBase
+import com.mairwunnx.projectessentials.core.api.v1.messaging.MessagingAPI
+import com.mairwunnx.projectessentials.core.api.v1.messaging.ServerMessagingAPI
+import com.mairwunnx.projectessentials.managers.KitManager
+import com.mairwunnx.projectessentials.validateAndExecute
+import com.mojang.brigadier.context.CommandContext
+import net.minecraft.command.CommandSource
+
+object KitCommand : CommandBase(kitLiteral, false) {
+    override val name = "kit"
+
+    fun kitList(context: CommandContext<CommandSource>) = 0.also {
+        validateAndExecute(context, "ess.kit.list", 0) { isServer ->
+            val list = KitManager.getKits().map { it.name }
+            if (isServer) {
+                ServerMessagingAPI.listAsResponse(list) { "Kits list" }
+            } else {
+                MessagingAPI.sendListAsMessage(context, list) { "Kits list" }
+                process(context)
+            }
+        }
+    }
+
+    fun airSelf(context: CommandContext<CommandSource>) = 0.also {
+
+    }
 }
