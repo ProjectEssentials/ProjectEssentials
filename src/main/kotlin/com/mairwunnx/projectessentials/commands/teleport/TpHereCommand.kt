9e8c785c4e5bdf96830749e775d45ea8ab3e258d
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-05-30 00:04:38 +0300

    tp-here command re-written.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/commands/teleport/TpHereCommand.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/commands/teleport/TpHereCommand.kt
@@ -1,74 +1,40 @@
 package com.mairwunnx.projectessentials.commands.teleport
 
-import com.mairwunnx.projectessentials.commands.CommandBase
-import com.mairwunnx.projectessentials.configurations.ModConfiguration.getCommandsConfig
-import com.mairwunnx.projectessentials.core.backlocation.BackLocationProvider
-import com.mairwunnx.projectessentials.core.helpers.throwPermissionLevel
-import com.mairwunnx.projectessentials.extensions.sendMsg
-import com.mairwunnx.projectessentials.permissions.permissions.PermissionsAPI
-import com.mojang.brigadier.CommandDispatcher
-import com.mojang.brigadier.builder.LiteralArgumentBuilder.literal
+import com.mairwunnx.projectessentials.commands.tpHereLiteral
+import com.mairwunnx.projectessentials.core.api.v1.MESSAGE_MODULE_PREFIX
+import com.mairwunnx.projectessentials.core.api.v1.commands.CommandAPI
+import com.mairwunnx.projectessentials.core.api.v1.commands.CommandBase
+import com.mairwunnx.projectessentials.core.api.v1.commands.back.BackLocationAPI
+import com.mairwunnx.projectessentials.core.api.v1.extensions.getPlayer
+import com.mairwunnx.projectessentials.core.api.v1.messaging.MessagingAPI
+import com.mairwunnx.projectessentials.core.api.v1.messaging.ServerMessagingAPI
+import com.mairwunnx.projectessentials.validateAndExecute
 import com.mojang.brigadier.context.CommandContext
 import net.minecraft.command.CommandSource
-import net.minecraft.command.Commands
-import net.minecraft.command.arguments.EntityArgument
-import net.minecraft.entity.player.ServerPlayerEntity
-import org.apache.logging.log4j.LogManager
 
-object TpHereCommand : CommandBase() {
-    private val logger = LogManager.getLogger()
-    private var config = getCommandsConfig().commands.tpHere
+object TpHereCommand : CommandBase(tpHereLiteral, false) {
+    override val name = "tp-here"
 
-    init {
-        command = "tphere"
-        aliases = config.aliases.toMutableList()
-    }
-
-    override fun reload() {
-        config = getCommandsConfig().commands.tpHere
-        aliases = config.aliases.toMutableList()
-        super.reload()
-    }
-
-    override fun register(dispatcher: CommandDispatcher<CommandSource>) {
-        super.register(dispatcher)
-        aliases.forEach { command ->
-            dispatcher.register(literal<CommandSource>(command)
-                .then(
-                    Commands.argument("player", EntityArgument.player()).executes {
-                        return@executes execute(it, EntityArgument.getPlayer(it, "player"))
-                    }
-                )
-            )
+    override fun process(context: CommandContext<CommandSource>) = 0.also {
+        validateAndExecute(context, "ess.teleport.tphere", 3) { isServer ->
+            if (isServer) {
+                ServerMessagingAPI.throwOnlyPlayerCan()
+            } else {
+                val target = CommandAPI.getPlayer(context, "target")
+                with(context.getPlayer()!!) {
+                    BackLocationAPI.commit(target)
+                    target.teleport(
+                        this.serverWorld,
+                        this.positionVec.x, this.positionVec.y, this.positionVec.z,
+                        this.rotationYaw, this.rotationPitch
+                    )
+                    MessagingAPI.sendMessage(
+                        context.getPlayer()!!,
+                        "${MESSAGE_MODULE_PREFIX}basic.tphere.success",
+                        args = *arrayOf(target.name.string)
+                    ).also { super.process(context) }
+                }
+            }
         }
-    }
-
-    override fun execute(
-        c: CommandContext<CommandSource>,
-        argument: Any?
-    ): Int {
-        super.execute(c, argument)
-
-        if (PermissionsAPI.hasPermission(senderName, "ess.tphere", senderIsServer)) {
-            val senderPosX = senderPlayer.posX
-            val senderPosY = senderPlayer.posY
-            val senderPosZ = senderPlayer.posZ
-            val senderWorld = senderPlayer.serverWorld
-            val senderYaw = senderPlayer.rotationYaw
-            val senderPitch = senderPlayer.rotationPitch
-
-            val target = argument as ServerPlayerEntity
-            BackLocationProvider.commit(target)
-            target.teleport(senderWorld, senderPosX, senderPosY, senderPosZ, senderYaw, senderPitch)
-
-            sendMsg(sender, "tphere.success", target.name.string)
-        } else {
-            throwPermissionLevel(senderName, command)
-            sendMsg(sender, "tphere.restricted", senderName)
-            return 0
-        }
-
-        logger.info("Executed command \"/$command\" from $senderName")
-        return 0
     }
 }
