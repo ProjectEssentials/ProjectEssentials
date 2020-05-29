64aa3e77258c31ec8a92384282da60131f9e543a
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-05-29 23:29:27 +0300

    call-all alias for tpa-all added.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/commands/teleport/TpaAllCommand.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/commands/teleport/TpaAllCommand.kt
@@ -14,6 +14,7 @@ import net.minecraft.command.CommandSource
 
 object TpaAllCommand : CommandBase(tpaAllLiteral) {
     override val name = "tpa-all"
+    override val aliases = listOf("call-all")
 
     override fun process(context: CommandContext<CommandSource>) = 0.also {
         validateAndExecute(context, "ess.teleport.tpaall", 2) { isServer ->
