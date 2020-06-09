e8235a616095d3e1ba86e5f04a27f7c987ad722b
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-09 20:10:48 +0300

    tpa-here command localization added.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/commands/teleport/TpaHereCommand.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/commands/teleport/TpaHereCommand.kt
@@ -5,6 +5,7 @@ import com.mairwunnx.projectessentials.core.api.v1.MESSAGE_MODULE_PREFIX
 import com.mairwunnx.projectessentials.core.api.v1.commands.CommandAPI
 import com.mairwunnx.projectessentials.core.api.v1.commands.CommandBase
 import com.mairwunnx.projectessentials.core.api.v1.extensions.getPlayer
+import com.mairwunnx.projectessentials.core.api.v1.extensions.playerName
 import com.mairwunnx.projectessentials.core.api.v1.messaging.MessagingAPI
 import com.mairwunnx.projectessentials.core.api.v1.messaging.ServerMessagingAPI
 import com.mairwunnx.projectessentials.managers.TeleportManager
@@ -21,8 +22,17 @@ object TpaHereCommand : CommandBase(tpaHereLiteral, false) {
     override fun process(context: CommandContext<CommandSource>) = 0.also {
         validateAndExecute(context, "ess.teleport.tpahere", 0) { isServer ->
             fun out(result: String) = MessagingAPI.sendMessage(
-                context.getPlayer()!!, "${MESSAGE_MODULE_PREFIX}basic.tpahere.$result"
-            )
+                context.getPlayer()!!, "${MESSAGE_MODULE_PREFIX}basic.tpahere.$result",
+                args = *arrayOf(CommandAPI.getPlayer(context, "target").name.string)
+            ).also {
+                if (result == "success") {
+                    MessagingAPI.sendMessage(
+                        CommandAPI.getPlayer(context, "target"),
+                        "${MESSAGE_MODULE_PREFIX}basic.tpahere.by.$result",
+                        args = *arrayOf(context.playerName())
+                    )
+                }
+            }
 
             if (isServer) {
                 ServerMessagingAPI.throwOnlyPlayerCan()
