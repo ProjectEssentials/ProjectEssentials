b84f1a380ca00c903a72b3157942720081c36b75
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-09 19:14:34 +0300

    KitCommand.kt give kit other implemented. Also command simplified.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/commands/KitCommand.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/commands/KitCommand.kt
@@ -1,10 +1,8 @@
 package com.mairwunnx.projectessentials.commands
 
-import com.mairwunnx.projectessentials.configurations.UserDataConfiguration
 import com.mairwunnx.projectessentials.core.api.v1.MESSAGE_MODULE_PREFIX
 import com.mairwunnx.projectessentials.core.api.v1.commands.CommandAPI
 import com.mairwunnx.projectessentials.core.api.v1.commands.CommandBase
-import com.mairwunnx.projectessentials.core.api.v1.configuration.ConfigurationAPI.getConfigurationByName
 import com.mairwunnx.projectessentials.core.api.v1.extensions.getPlayer
 import com.mairwunnx.projectessentials.core.api.v1.messaging.MessagingAPI
 import com.mairwunnx.projectessentials.core.api.v1.messaging.ServerMessagingAPI
@@ -16,10 +14,6 @@ import net.minecraft.command.CommandSource
 
 object KitCommand : CommandBase(kitLiteral, false) {
     override val name = "kit"
-
-    private val userDataConfiguration by lazy {
-        getConfigurationByName<UserDataConfiguration>("user-data")
-    }
 
     fun kitList(context: CommandContext<CommandSource>) = 0.also {
         validateAndExecute(context, "ess.kit.list", 0) { isServer ->
@@ -49,7 +43,7 @@ object KitCommand : CommandBase(kitLiteral, false) {
                     ).also { if (result == "success") process(context) }
                 }
 
-                when (KitManager.requestKit(player, kit)) {
+                when (KitManager.requestKit(player, player, kit)) {
                     Response.Success -> out("success")
                     Response.KitNoHasPermissions -> out("no_permission")
                     Response.KitNotFound -> out("not_found")
@@ -59,45 +53,38 @@ object KitCommand : CommandBase(kitLiteral, false) {
         }
     }
 
-    fun kitRefreshCooldown(context: CommandContext<CommandSource>) = 0.also {
-        validateAndExecute(context, "ess.kit.receive.other.cooldown.refresh", 4) { isServer ->
-            val kit = CommandAPI.getString(context, "kit-name")
-            val target = CommandAPI.getPlayer(context, "target")
-            val targetName = target.name.string
+    fun kitGetOther(context: CommandContext<CommandSource>) = 0.also {
+        validateAndExecute(context, "ess.kit.receive.other", 3) { isServer ->
+            if (isServer) {
+                ServerMessagingAPI.throwOnlyPlayerCan()
+            } else {
+                val kit = CommandAPI.getString(context, "kit-name")
+                val sender = context.getPlayer()!!
+                val target = CommandAPI.getPlayer(context, "target")
 
-            fun refreshFailed() {
-                if (isServer) {
-                    ServerMessagingAPI.response { "Player $targetName has no cooldown for kit $kit." }
-                } else {
+                fun out(result: String) {
                     MessagingAPI.sendMessage(
-                        context.getPlayer()!!,
-                        "${MESSAGE_MODULE_PREFIX}basic.kit.other.cooldown_refresh.error",
-                        args = *arrayOf(targetName, kit)
-                    )
+                        sender,
+                        "${MESSAGE_MODULE_PREFIX}basic.kit.other.$result",
+                        args = *arrayOf(kit)
+                    ).also {
+                        if (result == "success") {
+                            MessagingAPI.sendMessage(
+                                target,
+                                "${MESSAGE_MODULE_PREFIX}basic.kit.by",
+                                args = *arrayOf(sender.name.string)
+                            ).also { process(context) }
+                        }
+                    }
+                }
+
+                when (KitManager.requestKit(sender, target, kit)) {
+                    Response.Success -> out("success")
+                    Response.KitNoHasPermissions -> out("no_permission")
+                    Response.KitNotFound -> out("not_found")
+                    Response.KitTimeNotExpired -> out("kit_not_expired")
                 }
             }
-
-            userDataConfiguration.take().users.find {
-                it.name == targetName || it.uuid == target.uniqueID.toString()
-            }?.let { user ->
-                user.lastKitsDates.map { value ->
-                    value.split('@').let { it[0] to it[1] }
-                }.find { it.first == kit }?.let { expiredKit ->
-                    user.lastKitsDates.removeAll { expiredKit.first in it }.also {
-                        if (it) {
-                            if (isServer) {
-                                ServerMessagingAPI.response { "Kit $kit cooldown was removed for player $targetName." }
-                            } else {
-                                MessagingAPI.sendMessage(
-                                    context.getPlayer()!!,
-                                    "${MESSAGE_MODULE_PREFIX}basic.kit.other.cooldown_refresh.success",
-                                    args = *arrayOf(kit, targetName)
-                                )
-                            }
-                        } else refreshFailed()
-                    }
-                } ?: run { refreshFailed() }
-            } ?: run { refreshFailed() }
         }
     }
 }
