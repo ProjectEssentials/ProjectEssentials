64a8feb4e8ceb52c6260c6d5261325fb998c3099
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-09 19:15:30 +0300

    SuicideCommand.kt cleanup.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/commands/SuicideCommand.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/commands/SuicideCommand.kt
@@ -2,20 +2,15 @@ package com.mairwunnx.projectessentials.commands
 
 import com.mairwunnx.projectessentials.core.api.v1.SETTING_LOC_ENABLED
 import com.mairwunnx.projectessentials.core.api.v1.commands.CommandBase
-import com.mairwunnx.projectessentials.core.api.v1.configuration.ConfigurationAPI.getConfigurationByName
 import com.mairwunnx.projectessentials.core.api.v1.extensions.getPlayer
 import com.mairwunnx.projectessentials.core.api.v1.messaging.ServerMessagingAPI
-import com.mairwunnx.projectessentials.core.impl.configurations.GeneralConfiguration
+import com.mairwunnx.projectessentials.generalConfiguration
 import com.mairwunnx.projectessentials.validateAndExecute
 import com.mojang.brigadier.context.CommandContext
 import net.minecraft.command.CommandSource
 import net.minecraft.util.DamageSource
 
 object SuicideCommand : CommandBase(suicideLiteral) {
-    private val generalConfiguration by lazy {
-        getConfigurationByName<GeneralConfiguration>("general")
-    }
-
     override val name = "suicide"
 
     override fun process(context: CommandContext<CommandSource>) = 0.also {
