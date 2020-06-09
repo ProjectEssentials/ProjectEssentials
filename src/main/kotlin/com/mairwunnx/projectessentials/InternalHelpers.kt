7c05585de2be86a069a05d375132c78232e53d77
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-09 19:01:03 +0300

    `generalConfiguration` removed.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/InternalHelpers.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/InternalHelpers.kt
@@ -2,15 +2,11 @@ package com.mairwunnx.projectessentials
 
 import com.mairwunnx.projectessentials.core.api.v1.MESSAGE_MODULE_PREFIX
 import com.mairwunnx.projectessentials.core.api.v1.commands.CommandAliases
-import com.mairwunnx.projectessentials.core.api.v1.configuration.ConfigurationAPI.getConfigurationByName
 import com.mairwunnx.projectessentials.core.api.v1.extensions.getPlayer
 import com.mairwunnx.projectessentials.core.api.v1.messaging.MessagingAPI
 import com.mairwunnx.projectessentials.core.api.v1.permissions.hasPermission
-import com.mairwunnx.projectessentials.core.impl.configurations.GeneralConfiguration
 import com.mojang.brigadier.context.CommandContext
 import net.minecraft.command.CommandSource
-
-private val generalConfiguration = getConfigurationByName<GeneralConfiguration>("general")
 
 /**
  * Return true if command argument is alias of
