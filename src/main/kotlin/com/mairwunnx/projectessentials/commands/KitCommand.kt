ba52388d524c445188449022ff9e222547a80853
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-09 15:41:48 +0300

    `kitOtherGet` removed.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/commands/KitCommand.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/commands/KitCommand.kt
@@ -23,7 +23,7 @@ object KitCommand : CommandBase(kitLiteral, false) {
 
     fun kitList(context: CommandContext<CommandSource>) = 0.also {
         validateAndExecute(context, "ess.kit.list", 0) { isServer ->
-            val list = KitManager.getKits().map { it.name }
+            val list = KitManager.getKits().map { it.name }.toList()
             if (isServer) {
                 ServerMessagingAPI.listAsResponse(list) { "Kits list" }
             } else {
@@ -55,39 +55,6 @@ object KitCommand : CommandBase(kitLiteral, false) {
                     Response.KitNotFound -> out("not_found")
                     Response.KitTimeNotExpired -> out("kit_not_expired")
                 }
-            }
-        }
-    }
-
-    fun kitOtherGet(context: CommandContext<CommandSource>) = 0.also {
-        validateAndExecute(context, "ess.kit.receive.other", 3) { isServer ->
-            val kit = CommandAPI.getString(context, "kit-name")
-            val target = CommandAPI.getPlayer(context, "target")
-            val targetName = target.name.string
-
-            fun out(result: String, vararg args: String) {
-                if (isServer) {
-                    when (result) {
-                        "success" -> ServerMessagingAPI.response { "Kit $kit has been given to the player $targetName." }
-                        "no_permission" -> ServerMessagingAPI.response { "Player $targetName has no permission for getting kit $kit." }
-                        "not_found" -> ServerMessagingAPI.response { "Requested kit $kit was not found." }
-                        "kit_not_expired" -> ServerMessagingAPI.response { "Kit $kit cooldown is not expired for player $targetName." }
-                    }
-                } else {
-                    val player = context.getPlayer()!!
-                    MessagingAPI.sendMessage(
-                        player,
-                        "${MESSAGE_MODULE_PREFIX}basic.kit.other.$result",
-                        args = *args
-                    ).also { if (result == "success") process(context) }
-                }
-            }
-
-            when (KitManager.requestKit(target, kit)) {
-                Response.Success -> out("success", kit, targetName)
-                Response.KitNoHasPermissions -> out("no_permission", targetName, kit)
-                Response.KitNotFound -> out("not_found", kit)
-                Response.KitTimeNotExpired -> out("kit_not_expired", kit, targetName)
             }
         }
     }
