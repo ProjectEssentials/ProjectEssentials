9374d96279eda3ea9b72c33f0c7d2df5ac0a4f06
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-09 19:00:15 +0300

    general configuration added to global scope.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/GlobalScope.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/GlobalScope.kt
@@ -3,6 +3,11 @@ package com.mairwunnx.projectessentials
 import com.mairwunnx.projectessentials.configurations.KitsConfiguration
 import com.mairwunnx.projectessentials.configurations.UserDataConfiguration
 import com.mairwunnx.projectessentials.core.api.v1.configuration.ConfigurationAPI.getConfigurationByName
+import com.mairwunnx.projectessentials.core.impl.configurations.GeneralConfiguration
+
+val generalConfiguration by lazy {
+    getConfigurationByName<GeneralConfiguration>("general")
+}
 
 val userDataConfiguration by lazy {
     getConfigurationByName<UserDataConfiguration>("user-data").take()
